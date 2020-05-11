d4e2d1975e1f24f7f0d20bb739a11f0328d15f59
shivasharma1412 <shivasharma1412@gmail.com>
2020-05-11 16:59:44 +0530

    EWALLET

---
+++ E-Wallet/src/main/java/com/capgemini/ewallet/controller/WalletController.java
@@ -0,0 +1,42 @@
+package com.capgemini.ewallet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.ewallet.entity.WalletAccount;
+import com.capgemini.ewallet.entity.WalletUser;
+import com.capgemini.ewallet.service.WalletServiceImpl;
+
+
+@RestController
+@RequestMapping("/wallet")
+public class WalletController {
+
+
+@Autowired 
+WalletServiceImpl WalletService;
+
+
+
+@PostMapping(value = "/depositmoney", consumes = { "application/json" })
+public String depositmoney(@RequestBody WalletAccount input) {
+WalletAccount wallet = WalletService.findAccount(input.getAccountId());
+double amount = input.getAccountBalance();
+
+return WalletService.depositmoney(wallet, amount);
+}
+
+
+@GetMapping(value = "/showBalance/{id}")
+public WalletUser showBalance(@PathVariable int userid) {
+
+WalletUser user1 = WalletService.showBalance(userid);
+return user1;
+}
+
+}
